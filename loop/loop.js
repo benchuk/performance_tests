a93bf2330c8070a90a73a9eb2c7041ff1ce700c3
benchuk <benchuk@gmail.com>
2019-02-15 16:48:55 +0200

    .

--- loop/loop.js
+++ loop/loop.js
@@ -1,10 +1,11 @@
 var i;
 var sum = 0;
-console.time("test");
+
+console.time('test');
 for (i = 0; i < 10000000; i++) {
-    sum++;
+  sum++;
 }
-console.timeEnd("test");
-console.log(sum)
-console.log("loops 10000000");
-console.log("done");
+console.timeEnd('test');
+console.log(sum);
+console.log('loops 10000000');
+console.log('done');
